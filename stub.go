e0e5e40e613932d4770464b093b6f0d6ccf82d1b
Tim <6703801+timzifer@users.noreply.github.com>
2025-11-21 15:13:38 +0100

    Update stub.go

--- stub.go
+++ stub.go
@@ -13,13 +13,6 @@ type Value[T any] interface {
 	Keys() map[string]string
 }
 
-// Serializer definiert Methoden zur Konvertierung von Daten des Typs T.
-type Serializer[T any] interface {
-	Marshal(v T) ([]byte, error)
-	Unmarshal(data []byte, target *T) error // Nimmt Pointer auf T
-	Name() string                           // Gibt einen Namen zurück (z.B. für Logging)
-}
-
 type Receiver[T any] interface {
 	Receive(Value[T]) error
 }
